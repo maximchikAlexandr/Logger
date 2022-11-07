42a29a6c0154f582d8cfeea51f17e74d91f9f8c8
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-11-07 12:01:47 +0300

    Add methods __init__, logging_start, logging_end

--- logger.py
+++ logger.py
@@ -1,6 +1,15 @@
 from collections import defaultdict
+from timeit import default_timer
 
 
 class Logger:
-    pass
 
+    def __init__(self):
+        self.__logger_dict = defaultdict()
+        self.__temp_time = 0
+
+    def logging_start(self):
+        self.__temp_time = default_timer()
+
+    def logging_end(self, user_param):
+        self.__logger_dict[user_param] = self.__temp_time - default_timer()
