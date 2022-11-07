db692296b0b23733b4e667890a68ff400c05401a
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-11-07 12:28:49 +0300

    Add methods __default_timer, print_results

--- logger.py
+++ logger.py
@@ -6,10 +6,24 @@ class Logger:
 
     def __init__(self):
         self.__logger_dict = defaultdict()
-        self.__temp_time = 0
+        self.__default_timer()
 
     def logging_start(self):
-        self.__temp_time = default_timer()
+        self.__temp_time1 = default_timer()
 
     def logging_end(self, user_param):
-        self.__logger_dict[user_param] = self.__temp_time - default_timer()
+        self.__temp_time2 = default_timer()
+        if user_param in self.__logger_dict.keys():
+            self.__logger_dict[user_param] += self.__temp_time2 - self.__temp_time1
+        else:
+            self.__logger_dict[user_param] = self.__temp_time2 - self.__temp_time1
+        self.__default_timer()
+
+    def __default_timer(self):
+        self.__temp_time1 = 0
+        self.__temp_time2 = 0
+
+    def print_results(self):
+        print('Время работы:')
+        [print(f'"{key}" - {value} c') for key, value in self.__logger_dict.items()]
+        print('-------Конец-------')
